edaf726d67190fe465d8f3a8b349ce3f2ce2fb36
Pavan Kumar Reddy Sannala <pavanreddyr34@gmail.com>
2023-10-23 22:16:07 +0530

    new -changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="custom-home-container">
-      
-          <Home/>
+      <Home/>
     </div>
   );
 }
